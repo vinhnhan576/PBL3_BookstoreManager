dcec4225192646a0b32c30885548e9e724958481
Vinh Nhan <vinhnhan576@gmail.com>
2022-05-09 20:24:22 +0700

    fix

--- PBL3/Model1.Context.cs
+++ PBL3/Model1.Context.cs
@@ -15,24 +15,6 @@ namespace PBL3
     
     public partial class QLSPEntities : DbContext
     {
-       
-        private static QLSPEntities _instance;
-
-        public static QLSPEntities Instance
-        {
-            get
-            {
-                if (_instance == null)
-                {
-                    _instance = new QLSPEntities();
-                }
-                return _instance;
-            }
-            private set
-            {
-
-            }
-        }
         public QLSPEntities()
             : base("name=QLSPEntities")
         {
