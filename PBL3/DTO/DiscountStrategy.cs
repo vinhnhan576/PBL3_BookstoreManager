2969addc125e1d39a7386b43cb48c07027c5aad7
sumori <hoangthiphuonguyen1602@gmail.com>
2022-06-17 23:29:43 +0700

    update discount

---
+++ PBL3/DTO/DiscountStrategy.cs
@@ -0,0 +1,15 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using PBL3.Model;
+
+namespace PBL3.DTO
+{
+    public interface DiscountStrategy
+    {
+        double DoDiscount(List<ReceiptDetailView> list);
+        double getPromotedDiscount(List<ReceiptDetailView> list);
+    }
+}
